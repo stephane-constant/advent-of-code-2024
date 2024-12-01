97722006b436146d6defdb4116ee605d4100e1ab
Stephane CONSTANT <stephane.constant@gmail.com>
2024-12-01 22:51:25 +0100

    Day1 : parts 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,49 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun readLocations(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val leftLocations = mutableListOf<Int>()
+        val rightLocations = mutableListOf<Int>()
+
+        input.forEach { line ->
+            val locations = line.split("   ")
+            leftLocations.add(locations[0].toInt())
+            rightLocations.add(locations[1].toInt())
+        }
+
+        return Pair(leftLocations, rightLocations)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (leftLocations, rightLocations) = readLocations(input)
+
+        leftLocations.sort()
+        rightLocations.sort()
+
+        var totalDistance = 0
+        for (i in 0 until leftLocations.size) {
+            totalDistance += abs(leftLocations[i] - rightLocations[i])
+        }
+
+        return totalDistance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (leftLocations, rightLocations) = readLocations(input)
+
+        var totalSimilarityScore = 0
+        leftLocations.forEach { location ->
+            totalSimilarityScore += location * rightLocations.count { l -> l == location }
+        }
+
+        return totalSimilarityScore
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("3   9", "5   2")) == 1+4)
+    check(part1(readInput("Day01_test")) == 11)
+    "Part1 result = ${part1(readInput("Day01"))}".println()
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    check(part2(readInput("Day01_test")) == 31)
+    "Part2 result = ${part2(readInput("Day01"))}".println()
 }
