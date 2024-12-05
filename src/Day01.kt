08cd16f555ce48a6e13b28cc9775c6122be78e56
Stephane CONSTANT <stephane.constant@gmail.com>
2024-12-05 02:13:01 +0100

    Day01 nicer with Long

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,26 +2,26 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun readLocations(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
-        val leftLocations = mutableListOf<Int>()
-        val rightLocations = mutableListOf<Int>()
+    fun readLocations(input: List<String>): Pair<MutableList<Long>, MutableList<Long>> {
+        val leftLocations = mutableListOf<Long>()
+        val rightLocations = mutableListOf<Long>()
 
         input.forEach { line ->
             val locations = line.split("   ")
-            leftLocations.add(locations[0].toInt())
-            rightLocations.add(locations[1].toInt())
+            leftLocations.add(locations.first().toLong())
+            rightLocations.add(locations.last().toLong())
         }
 
         return Pair(leftLocations, rightLocations)
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val (leftLocations, rightLocations) = readLocations(input)
 
         leftLocations.sort()
         rightLocations.sort()
 
-        var totalDistance = 0
+        var totalDistance = 0L
         for (i in 0 until leftLocations.size) {
             totalDistance += abs(leftLocations[i] - rightLocations[i])
         }
@@ -29,10 +29,10 @@ fun main() {
         return totalDistance
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val (leftLocations, rightLocations) = readLocations(input)
 
-        var totalSimilarityScore = 0
+        var totalSimilarityScore = 0L
         leftLocations.forEach { location ->
             totalSimilarityScore += location * rightLocations.count { l -> l == location }
         }
@@ -40,10 +40,10 @@ fun main() {
         return totalSimilarityScore
     }
 
-    check(part1(listOf("3   9", "5   2")) == 1+4)
-    check(part1(readInput("Day01_test")) == 11)
+    check(part1(listOf("3   9", "5   2")) == 5L)
+    check(part1(readInput("Day01_test")) == 11L)
     "Part1 result = ${part1(readInput("Day01"))}".println()
 
-    check(part2(readInput("Day01_test")) == 31)
+    check(part2(readInput("Day01_test")) == 31L)
     "Part2 result = ${part2(readInput("Day01"))}".println()
 }
